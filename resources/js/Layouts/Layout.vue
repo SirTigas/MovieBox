2fef81fb7cfba152c1a04c615795692ba268a46d
Tiago Rodrigues <tiagocardoso2077@gmail.com>
2026-03-14 13:59:20 -0300

    first commit

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        HEADER
+    </div>
+
+    <main>
+        <slot
+
+        />
+    </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{--Font Awesome--}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    @vite('resources/js/app.js')
+    @inertiaHead
+    @routes
+</head>
+<body>
+    @inertia
+</body>
+</html>
